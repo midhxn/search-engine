6a46b9b36a34cda144d3d7871f11a9b7f91c661f
midhun <midhunn311@gmail.com>
2022-05-26 19:07:49 +0530

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+export const NavBar = () =>{
+    return(
+        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+            <div className="flex justify-between items-center space-x-5 w-screen">
+                <Link to="/">
+                    <p className="text-2xl"></p>
+                </Link>
+            </div>
+        </div>
+        );
+}
