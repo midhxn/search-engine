ce99bd07066da42b1a4f8841c8bd39aa980f4d89
midhun <midhunn311@gmail.com>
2022-05-26 19:07:30 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { Footer } from './components/Footer';
+import { NavBar } from './components/Navbar';
+import { Routes } from './components/Routes';
 
 const App = () => {
+
+    const [darkTheme,setDarkTheme] = useState(false);
     return(
-        <div>
-            <h1>Seacrch engine</h1>
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className="bg-gray-100:bg-grap-900 dark:text-gray-200 min-h-screen">
+                <NavBar/>
+                <Routes/>
+                <Footer/>
+            </div>
         </div>
         );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {Navbar} from './components/Navbar';
+import {Footer} from './components/Footer';
 import App from './App';
+import './global.css';
 
 ReactDOM.render(<App />,document.getElementById('root'));
