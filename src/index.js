4b6dbe33511312a764c09d333dd1d1a708f241e8
midhun <midhunn311@gmail.com>
2022-05-27 21:35:17 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+
 import {Navbar} from './components/Navbar';
 import {Footer} from './components/Footer';
 import App from './App';
 import './global.css';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+document.getElementById('root')
+);
