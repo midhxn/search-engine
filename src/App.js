41b5425d01110f5928af432438b086fc12f8c66c
midhun <midhunn311@gmail.com>
2022-05-27 21:35:09 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const App = () => {
     const [darkTheme,setDarkTheme] = useState(false);
     return(
         <div className={darkTheme ? 'dark' : ''}>
-            <div className="bg-gray-100:bg-grap-900 dark:text-gray-200 min-h-screen">
-                <NavBar/>
+            <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+                <NavBar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
                 <Routes/>
                 <Footer/>
             </div>
