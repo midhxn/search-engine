faf6f8db474de643de70785e973f5994f79d817e
midhun <midhunn311@gmail.com>
2022-05-28 23:48:02 +0530

    commit day4

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Routes as Switch, Route, Redirect} from 'react-router-dom';
 
 export const Routes = () =>{
     return(
-        <div>
-            <h1>Routes</h1>
+        <div className="p-4">
+           <Switch>
+               <Route exact path="/">
+                   <Redirect to="/search"/>
+               </Route>
+           </Switch>
         </div>
         );
 }
