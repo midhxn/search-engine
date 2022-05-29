82da277f341e44ee2b7f7325303a8c84b55237ac
midhun <midhunn311@gmail.com>
2022-05-29 23:39:16 +0530

    footer styles added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Footer = () =>{
     return(
-        <div>
-            <h1>Footer</h1>
+        <div classname="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+            <h1>Since 2021</h1>
         </div>
         );
 }
