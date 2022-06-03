bcffb8b7e819fc844f9813f63da38c41c2ba9936
midhun <midhunn311@gmail.com>
2022-06-04 00:15:09 +0530

    added context provider💪

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import {BrowserRouter as Router} from 'react-router-dom';
 
 import {Navbar} from './components/Navbar';
 import {Footer} from './components/Footer';
+import { ResultContextProvider } from './context/ResultContextProvider';
 import App from './App';
 import './global.css';
 
 ReactDOM.render(
+<ResultContextProvider>
     <Router>
         <App />
     </Router>,
+</ResultContextProvider>,
 document.getElementById('root')
 );
