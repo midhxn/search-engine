de5c43a800ed25eb50d385780792ff2322daf2f3
midhun <midhunn311@gmail.com>
2022-06-04 00:34:39 +0530

    created loading components🤏

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as Loader from 'react-loader-spinner';
+
+export const Loading = () => {
+  return (
+    <div className="flex justify-center items-center">
+        <Loader.TailSpin type="puff" color="#00BFFF" height={550} width={82}/>
+    </div>
+  )
+}
