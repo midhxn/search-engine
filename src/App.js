72060694fd08d7c815f11c14138b338f4bac3650
midhun <midhunn311@gmail.com>
2022-06-04 00:33:57 +0530

    Updates in main file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import { Footer } from './components/Footer';
 import { NavBar } from './components/Navbar';
-import { Routes } from './components/Routes';
+import { NavRoutes } from './components/NavRoutes';
 
 const App = () => {
 
@@ -10,7 +10,7 @@ const App = () => {
         <div className={darkTheme ? 'dark' : ''}>
             <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
                 <NavBar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
-                <Routes/>
+                <NavRoutes/>
                 <Footer/>
             </div>
         </div>
