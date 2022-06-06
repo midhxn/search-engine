4f207905d0b85db2aaedbc79a94497358647fe0c
midhun <midhunn311@gmail.com>
2022-06-06 23:53:08 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () =>{
     return(
-        <div classname="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+        <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
             <h1>Since 2021</h1>
         </div>
         );
