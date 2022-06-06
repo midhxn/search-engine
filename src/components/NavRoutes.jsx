c610dd4657636f1ec5e139d2a28ae1cf510d8cc6
midhun <midhunn311@gmail.com>
2022-06-06 23:53:20 +0530

    update

--- src/components/NavRoutes.jsx
+++ src/components/NavRoutes.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Routes,Route,Navigate} from 'react-router-dom';
-import Results from './Results';
+import {Results} from './Results';
 
 export const NavRoutes = () =>{
     return(
         <div className="p-4">
             <Routes>
                 <Route exact path="/" element={<Navigate to="/search" />} />
+                <Route exact path="/search" element={<Results/>} />
+                <Route path="/images" element={<Results/>} />
+                <Route path="/news" element={<Results/>} />
+                <Route path="/videos" element={<Results/>} />
            
             </Routes>
         </div>
